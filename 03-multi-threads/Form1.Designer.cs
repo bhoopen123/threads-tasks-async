9bad54ad512049acad484954687d4866141f23fd
Bhoopendra Sharma <bhoopendras73@gmail.com>
2025-12-28 12:15:12 +0530

    chore : multiple threads

---
+++ 03-multi-threads/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace _03_multi_threads
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            lblMessage = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(150, 292);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Message 1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(450, 292);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Message 2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // lblMessage
+            // 
+            lblMessage.AutoSize = true;
+            lblMessage.Location = new Point(289, 152);
+            lblMessage.Name = "lblMessage";
+            lblMessage.Size = new Size(66, 15);
+            lblMessage.TabIndex = 2;
+            lblMessage.Text = "lblMessage";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblMessage);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label lblMessage;
+    }
+}
---
+++ 03-multi-threads/Form1.cs
@@ -0,0 +1,50 @@
+namespace _03_multi_threads
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // This is a big fat task that can be run on a different threat
+            // Also it blocks the UI thread until it completes
+            //ShowMessage("First Message", 3000);
+
+            // So, we can use Thread and assign this task to the thread to complete
+            Thread thread = new Thread(() => ShowMessage("First Message", 3000));
+            thread.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //ShowMessage("Second Message", 5000);
+
+            Thread thread = new Thread(() => ShowMessage("Second Message", 5000));
+            thread.Start();
+        }
+
+        private void ShowMessage(string message, int delay)
+        {
+            Thread.Sleep(delay);
+            //lblMessage.Text = message;
+
+            // Assuming you are in a background thread and want to update lblMessage
+            if (lblMessage.InvokeRequired)
+            {
+                // If we are on a different thread, use Invoke to marshal the call back to the UI thread
+                lblMessage.Invoke((MethodInvoker)delegate
+                {
+                    lblMessage.Text = message;
+                });
+            }
+            else
+            {
+                // Otherwise, we are already on the UI thread and can update directly
+                lblMessage.Text = message;
+            }
+        }
+    }
+}
