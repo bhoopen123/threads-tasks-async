5f37628addeb931194d80f8c78f47d1f15537ba2
Bhoopendra Sharma <bhoopendras73@gmail.com>
2026-01-01 13:10:56 +0530

    chore : SemaphoreSlim example

---
+++ 09-semaphore/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Queue<string?> requestQueue = new Queue<string?>();
+bool isRunning = true;
+
+SemaphoreSlim semaphore = new SemaphoreSlim(initialCount: 3, maxCount: 3); // means 3 threads can run parellel
+object queueLock = new object();
+
+// 2. Start Processing Monitor Queue requests
+Thread monitorThread = new Thread(MonitorQueue);
+monitorThread.Start();
+
+Console.WriteLine("Hello,  Server is runing, Type 'exit' to stop.");
+// 1. Enque the input request
+while (true)
+{
+    string? input = Console.ReadLine();
+    if (input?.ToLower() == "exit")
+    {
+        isRunning = false;
+        monitorThread.Join();
+        break;
+    }
+
+    QueueRequest(input);
+    //ProcessRequest(input); // Processing input requests on the Main thread will block the subsequent requests until the current request is processed
+}
+
+void QueueRequest(string? input)
+{
+    lock (queueLock)
+    {
+        requestQueue.Enqueue(input);
+    }
+}
+
+void MonitorQueue()
+{
+    while (isRunning || requestQueue.Count > 0)
+    {
+        if (requestQueue.Count > 0)
+        {
+            string? inputRequest = null;
+
+            lock (queueLock)
+            {
+                inputRequest = requestQueue.Dequeue();
+            }
+
+            semaphore.Wait(); // instead of creating a new thread everytime, we use semaphore to ensure that only 3 thread can be created parallely. 
+            // once, any of the previous thread is completed 
+            // we will release the semaphore, so a new thread can be created
+            Thread processingThread = new Thread(() => ProcessRequest(inputRequest));
+            processingThread.Start();
+        }
+
+        Thread.Sleep(100);
+    }
+}
+
+void ProcessRequest(string? input)
+{
+    try
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine($"Processed request : {input}");
+    }
+    finally
+    {
+        int previousCount = semaphore.Release();
+        Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId} released the semaphore. Previous count is {previousCount}.");
+    }
+}
