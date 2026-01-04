b0142f8cd92a3cb6a0e0d83cf2fd4c8f1d4474f2
Bhoopendra Sharma <bhoopendras73@gmail.com>
2026-01-05 00:12:10 +0530

    feat : continueChain and Unwrap

---
+++ 32-continueChain-unwrap/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+
+using var client = new HttpClient();
+
+// first task to get Pokemon list
+var taskPokemonListJson = client.GetStringAsync("https://pokeapi.co/api/v2/pokemon");
+
+// Continuation task to get First Pokemon url
+var taskGetFirstPokemonUrl = taskPokemonListJson.ContinueWith(t =>
+{
+    var result = t.Result;
+    var doc = JsonDocument.Parse(result);
+    JsonElement root = doc.RootElement;
+    JsonElement results = root.GetProperty("results");
+    JsonElement firstPokemon = results[0];
+    return firstPokemon.GetProperty("url").ToString();
+});
+
+// further Continuation task to get Pokemon details
+var taskGetFirstPokemonDetailsJson = taskGetFirstPokemonUrl.ContinueWith(t =>
+{
+    var result = t.Result;
+    return client.GetStringAsync(result);
+}).Unwrap();    // taskGetFirstPokemonUrl.ContinueWith returns Task<Task<>>, so need to use Unwrap() function to get Task<>
+
+_ = taskGetFirstPokemonDetailsJson.ContinueWith(t =>
+{
+    var result = t.Result;
+    var doc = JsonDocument.Parse(result);
+    JsonElement root = doc.RootElement;
+    Console.WriteLine($"Name: {root.GetProperty("name").ToString()}");
+    Console.WriteLine($"Weight: {root.GetProperty("weight").ToString()}");
+    Console.WriteLine($"Height: {root.GetProperty("height")}");
+});
+
+Console.WriteLine("This is the end of the program.");
+
+Console.ReadLine();
