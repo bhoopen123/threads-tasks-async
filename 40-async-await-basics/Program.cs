6fe4ce3ef10bdef95007e0ffca11b82bee9b3645
Bhoopendra Sharma <bhoopendras73@gmail.com>
2026-01-10 23:35:32 +0530

    async await basics

---
+++ 40-async-await-basics/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, async-await !");
+
+Console.WriteLine("Starting to do work.");
+await WorkAsync();
+Console.WriteLine("Fetching data...");
+var data = await FetchDataAsync();
+Console.WriteLine($"Fetched : {data}");
+
+Console.WriteLine("Press enter to exit.");
+Console.ReadLine();
+
+static async Task WorkAsync()
+{
+    await Task.Delay(2000);
+
+    Console.WriteLine("Work is done.");
+}
+
+static async Task<string> FetchDataAsync()
+{
+    await Task.Delay(2000);
+
+    return "My complex data";
+}
