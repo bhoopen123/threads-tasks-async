e98246eaee7a254e4d19c02c4040f101ba9af289
Bhoopendra Sharma <bhoopendras73@gmail.com>
2026-01-13 00:14:09 +0530

    Parallel Loop Result

---
+++ 54-parallel-loop-result/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Parallel loop result !!");
+
+Console.WriteLine("When doing Break !!");
+ParallelLoopResult result = Parallel.For(0, 20, (i, state) =>
+{
+    Console.WriteLine($"Iteration {i}");
+
+    if (i == 10)
+    {
+        Console.WriteLine("Break called at iteration " + i);
+        state.Break();
+    }
+});
+
+Console.WriteLine($"IsCompleted: {result.IsCompleted}");
+Console.WriteLine($"LowestBreakIteration: {result.LowestBreakIteration}");
+
+Console.WriteLine();
+
+Console.WriteLine("When doing Stop !!");
+
+ParallelLoopResult result1 = Parallel.For(0, 20, (i, state) =>
+{
+    Console.WriteLine($"Iteration {i}");
+
+    if (i == 10)
+    {
+        Console.WriteLine("Stop called at iteration " + i);
+        state.Stop();
+    }
+});
+
+Console.WriteLine($"IsCompleted: {result1.IsCompleted}");
+Console.WriteLine($"LowestBreakIteration: {result1.LowestBreakIteration}");
+
+Console.ReadLine();
