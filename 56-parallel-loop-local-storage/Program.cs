67d2e74e80b50dd6002471d09b7e2bec2f2424cd
Bhoopendra Sharma <bhoopendras73@gmail.com>
2026-01-15 00:42:32 +0530

    parallel loop : local storage

---
+++ 56-parallel-loop-local-storage/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] array = Enumerable.Range(1, 1000000).ToArray();
+
+int sum = 0;
+
+object lockSum = new object();
+
+Parallel.For(
+    0, array.Length,
+    () => 0,    // initialized the Thread Local variable
+    (i, state, threadLocalStorage) =>   // using thread loal variable by parameter name 'threadLocalStorage'
+    {
+        threadLocalStorage += array[i];
+        return threadLocalStorage;
+    },
+    localFinally: lf => // aggregate that using shared variable 
+    {
+        lock (lockSum)
+        {
+            sum += lf;
+            Console.WriteLine($"The task id : {Task.CurrentId}");
+        }
+    });
+
+Console.WriteLine($"The sum is {sum}");
+
+Console.ReadLine();
