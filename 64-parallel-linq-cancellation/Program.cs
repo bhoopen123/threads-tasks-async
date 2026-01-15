7fb3bac3529e87f461918a102221ae55a2a9c08e
Bhoopendra Sharma <bhoopendras73@gmail.com>
2026-01-15 19:06:10 +0530

    parallel linq Cancellation

---
+++ 64-parallel-linq-cancellation/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, PLINQ Cancellation !");
+
+var items = Enumerable.Range(1, 200);
+
+using var cts = new CancellationTokenSource();
+
+var evenNumbers = items.AsParallel()
+    .WithCancellation(cts.Token)
+    .Where(i =>
+    {
+        //Console.WriteLine($"Processing number {i} by Thread Id :    {Thread.CurrentThread.ManagedThreadId}");
+
+        return i % 2 == 0;
+    });
+
+
+try
+{
+    evenNumbers.ForAll(even =>
+    {
+        if (even > 5) cts.Cancel();
+
+        Console.WriteLine($"Even Number {even} by Thread {Thread.CurrentThread.ManagedThreadId}");
+    });
+}
+catch (OperationCanceledException ex)
+{
+    Console.WriteLine("Query is cancelled...");
+}
+catch (AggregateException ex)
+{
+    Console.WriteLine("Error during Processing Number");
+
+    ex.Handle(err =>
+    {
+        Console.WriteLine(err.Message);
+        return true;
+    });
+}
+Console.ReadLine();
