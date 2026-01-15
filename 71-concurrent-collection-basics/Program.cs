93985b29cf23476daf51189e6f4addfe108b5694
Bhoopendra Sharma <bhoopendras73@gmail.com>
2026-01-15 19:19:44 +0530

    concurrent collection basics

---
+++ 71-concurrent-collection-basics/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Concurrent;
+
+Console.WriteLine("Hello, Concurrent Collection basics !!");
+
+var queue = new ConcurrentQueue<int>();
+
+queue.Enqueue(1);
+queue.Enqueue(2);
+queue.Enqueue(3);
+
+// there is no Dequeue in Concurrent Collection
+if (queue.TryDequeue(out var result))
+{
+    Console.WriteLine($"First value in Queue is {result}");
+}
+
+var stack = new ConcurrentStack<int>();
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+
+if (stack.TryPop(out var result1))
+{
+    Console.WriteLine($"First value in Stack is {result1}");
+}
+
+Console.Read();
+
