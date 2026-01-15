88b53c107f6619bcdaf49ab1543b674992797b61
Bhoopendra Sharma <bhoopendras73@gmail.com>
2026-01-15 15:26:19 +0530

    parallel LINQ : Exception handling

---
+++ 63-parallel-linq-exception/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, PLINQ exception !!");
+
+var items = Enumerable.Range(1, 20);
+
+var evenNumbers = items.AsParallel().Where(i =>
+{
+    Console.WriteLine($"Precessing number {i} by Thread Id : {Thread.CurrentThread.ManagedThreadId}");
+
+    if (i == 5) throw new InvalidDataException("This is intentional error at 5");
+
+    if (i == 10) throw new InvalidOperationException("This is intentional error at 10");
+
+    return i % 2 == 0;
+});
+
+try
+{
+    evenNumbers.ForAll(even =>
+    {
+        Console.WriteLine($"Even Number {even} by Thread {Thread.CurrentThread.ManagedThreadId}");
+    });
+}
+catch (AggregateException ex)
+{
+    Console.WriteLine("Error during Processing Number");
+
+    ex.Handle(err =>
+    {
+        Console.WriteLine(err.Message);
+        return true;
+    });
+}
+Console.ReadLine();
